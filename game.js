08820823921efbda1cfb200b8ee3c1cb35ede073
zapnaprogramer <121470682+zapnaprogramer@users.noreply.github.com>
2025-08-11 06:50:22 +0100

    Update game.js
    added the game function

--- game.js
+++ game.js
@@ -1,55 +1,36 @@
-
-
-    
-  
-
-
 function randChoice(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) ) + min;}
-  
-function Thebotchoice(){
-   var botchoice ;
-   let result = randChoice(1,3);
- //   console.log("Random Number: ", result);
-    
-    if (result == 1){
-        botchoice = "Paper";
-    }else if (result == 2){
-        botchoice = "scissors";
-   
-    } else {
-        botchoice = "rock";
-        
-    }
-    
-    return botchoice;
-    
-    }
-function humanchoice()   {
-let userChoice = prompt("Choose either Rock, Paper, Scissors: ");
-return userChoice;
-    
-    
-}    
-  
- 
-function game() {
-if (Thebotchoice() == "rock" && humanchoice() == "paper"){
-    return("Computer Won");
-}
-    
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function Thebotchoice() {
+  let result = randChoice(1, 3);
+  if (result == 1) return "Paper";
+  if (result == 2) return "scissors";
+  return "rock";
+}
 
- console.log(Thebotchoice());
- 
- console.log(humanchoice())
- console.log(game())
- 
+function humanchoice() {
+  let userChoice = prompt("Choose either Rock, Paper, Scissors: ");
+  return userChoice;
+}
 
+function game(bot, human) {
+  if (
+    (bot === "rock" && human === "scissors") ||
+    (bot === "scissors" && human === "Paper") ||
+    (bot === "Paper" && human === "rock")
+  ) {
+    return "Computer Won";
+  } else if (bot === human) {
+    return "It's a tie!";
+  } else {
+    return "You Won!";
+  }
+}
 
-    
+let bot = Thebotchoice();
+let human = humanchoice();
 
-
-
-//console.log("Hello world")
+console.log("Computer chose:", bot);
+console.log("You chose:", human);
+console.log(game(bot, human));
